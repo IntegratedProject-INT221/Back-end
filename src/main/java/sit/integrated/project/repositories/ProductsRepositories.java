f08c806ac2c509c937c8b09d4488b15df6ded441
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2021-04-30 18:19:35 +0700

    [uadate]model , repository

--- src/main/java/sit/integrated/project/repositories/ProductsRepositories.java
+++ src/main/java/sit/integrated/project/repositories/ProductsRepositories.java
@@ -1,7 +1,11 @@
 package sit.integrated.project.repositories;
 
+
+import org.springframework.data.jpa.repository.Query;
 import sit.integrated.project.models.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductsRepositories extends JpaRepository<Products,Integer> {
+    @Query(value = "select MAX(productId) FROM Products ")
+    public int productLatestId();
 }
