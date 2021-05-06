a29e20c82c1bc1daa579ff87db76f2e941df1327
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2021-05-06 20:44:05 +0700

    [add] mkdir file

--- src/main/java/sit/integrated/project/int221ProjectApplication.java
+++ src/main/java/sit/integrated/project/int221ProjectApplication.java
@@ -1,13 +1,23 @@
 package sit.integrated.project;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
-public class int221ProjectApplication {
+public class int221ProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(int221ProjectApplication.class, args);
 	}
+    @Override
+	public void  run(String... args) throws  Exception{
+		String dir = "./images";
+		File file = new File(dir);
+		file.mkdirs();
 
+
+	}
 }
