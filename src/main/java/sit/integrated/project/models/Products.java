ac5e8432dfa489ff78980868abe71d6a7876651e
Thongtong <THONGTONG.tg@mail.kmutt.ac.th>
2021-05-10 14:35:47 +0700

    [update]model

--- src/main/java/sit/integrated/project/models/Products.java
+++ src/main/java/sit/integrated/project/models/Products.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 @Entity
 @Table(name = "Products")
